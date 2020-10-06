dab6254c9772bcfe974527f0dfbfc6defa7a640c
Lucas <lukinhasmlopes@hotmail.com>
2020-10-06 00:47:59 -0300

    Removed reload command since its buggy
    Not implemented yet!

--- src/me/luucx7/simplexchat/cmds/SimplexCmd.java
+++ src/me/luucx7/simplexchat/cmds/SimplexCmd.java
@@ -5,10 +5,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-import me.luucx7.simplexchat.SimplexChat;
-import me.luucx7.simplexchat.core.managers.CanaisManager;
-import net.md_5.bungee.api.ChatColor;
-
 public class SimplexCmd implements CommandExecutor {
 
 	@Override
@@ -17,22 +13,6 @@ public class SimplexCmd implements CommandExecutor {
 			s.sendMessage("§b§lSimplexChat §r§b"+Bukkit.getServer().getPluginManager().getPlugin("SimplexChat").getDescription().getVersion()+" by Luucx7");
 			return true;
 		}
-		if (args[0].equalsIgnoreCase("reload")) {
-			if (!(s.hasPermission("simplexchat.reload"))) {
-				s.sendMessage(getString("no_permission"));
-				return true;
-			}
-			
-			CanaisManager.disable();
-			SimplexChat.instance.reloadConfig();
-			CanaisManager.load();
-			
-			s.sendMessage(getString("plugin_reloaded"));
-		}
 		return false;
-	}
-	
-	private static String getString(String path) {
-		return ChatColor.translateAlternateColorCodes('&', SimplexChat.instance.getConfig().getString(path));
 	}
 }
